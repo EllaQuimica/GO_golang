a9faa58f28945223c6a8c0a2b09bb804f85c5dbe
EllaQuimica <ellaquimica@gmail.com>
2020-06-06 14:32:08 -0300

    Check strconv.Atoi input String return int

--- calculator.go
+++ calculator.go
@@ -19,7 +19,12 @@ func main() {
 	values := strings.Split(operation, "+")
 	fmt.Println(values)
 	fmt.Println(values[0] + values[1])
-	operator1, _ := strconv.Atoi(values[0])
+	operator1, error1 := strconv.Atoi(values[0])
+	//verificamos que el operador 1 sea un int
+	if error1 != nil {
+		fmt.Println(error1)
+	}
+	fmt.Println(operator1)
 	operator2, _ := strconv.Atoi(values[1])
 	fmt.Println(operator1 + operator2)
 }
