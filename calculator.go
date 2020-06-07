d62193f99eb3c76bd7098cc3cf4575f343298fed
EllaQuimica <ellaquimica@gmail.com>
2020-06-07 04:38:35 -0300

    Use Switch with operator +, -, *  and  /

--- calculator.go
+++ calculator.go
@@ -9,22 +9,35 @@ import (
 )
 
 func main() {
-	//bufio es una libreria para scanener
-	//newScanner crea un nueva instancia de scan que recibe como input la termina;
+
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	operation := scanner.Text()
 	fmt.Println(operation)
-	//hacemos Split en el string que recibimos
-	values := strings.Split(operation, "+")
+
+	operator := "+"
+	values := strings.Split(operation, operator)
 	fmt.Println(values)
 	fmt.Println(values[0] + values[1])
 	operator1, error1 := strconv.Atoi(values[0])
-	//verificamos que el operador 1 sea un int
 	if error1 != nil {
 		fmt.Println(error1)
 	}
 	fmt.Println(operator1)
+
 	operator2, _ := strconv.Atoi(values[1])
-	fmt.Println(operator1 + operator2)
+
+	switch operator {
+	case "+":
+		fmt.Println("Sum: ",operator1 + operator2)
+	case "-":
+		fmt.Println("Rest: ",operator1 - operator2)
+	case "*":
+		fmt.Println("Product: ",operator1 * operator2)
+	case "/":
+		fmt.Println("Division: ", operator1 / operator2)
+	default:
+		fmt.Println(operator, "Not can support")
+	}
+
 }
